0335430acab27fa289205b4d827f866e58c9f6f9
wsievern <118843829+wsievern@users.noreply.github.com>
2022-12-26 11:35:54 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import kivy
+from kivy.lang import Builder
+from kivymd.app import MDApp
+from kivy.core.window import Window
+kivy.require('2.0.0')
+
+
+Window.size = (500, 1000)
+
+
+class BalletApp(MDApp):
+    def build(self):
+        self.root = Builder.load_file("ballet.kv")
+
+
+# if __name__ == '__main__':
+# BalletApp().run()
+
+
+BalletApp().run()
---
+++ navigation_screen_manager.py
@@ -0,0 +1,55 @@
+from kivy.uix.screenmanager import ScreenManager
+from kivymd.uix.relativelayout import MDRelativeLayout
+from kivy.uix.label import Label
+from kivy.properties import BooleanProperty
+
+
+class NavigationScreenManager(ScreenManager):
+    screen_stack = []
+
+
+
+
+
+    def push(self, screen_name):
+        if screen_name not in self.screen_stack:
+            self.screen_stack.append(self.current)
+            self.transition.direction = "left"
+            self.current = screen_name
+
+    def pop(self):
+        if len(self.screen_stack) > 0:
+            screen_name = self.screen_stack[-1]
+            del self.screen_stack[-1]
+            self.transition.direction = "right"
+            self.current = screen_name
+
+    def tempo_tap(self):
+        print("tapped")
+
+
+
+
+
+    '''def play_audio(self):
+        self.sound = Piece.loaded_piece
+        #self.song_title = self.song_list[random.randrange(0, self.song_count)]
+        #print(self.song_title)
+        #self.sound = SoundLoader.load('{}/{}'.format(self.music_dir, self.song_title))
+        self.sound.play()
+        self.play_enabled = False
+        self.stop_enabled = True'''
+
+
+
+    def display_title(self):
+        layout = MDRelativeLayout(md_bg_color=[0, 0.5, 1, 1])
+        self.songlabel = Label(pos_hint={'center_x': 0.5, 'center_y': .96},
+                               size_hint=(1, 1),
+                               font_size=30)
+        layout.add_widget(self.songlabel)
+        self.songlabel.text = "===== Playing ~ ====="
+        return layout
+
+
+
