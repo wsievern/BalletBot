4ae5c2e21736234341052ebe4d074bb4803846ec
wsievern <118843829+wsievern@users.noreply.github.com>
2022-12-26 23:52:11 -0600

    Add files via upload

---
+++ playback.py
@@ -0,0 +1,53 @@
+from kivy.lang import Builder
+from kivy.properties import BooleanProperty
+from kivy.uix.floatlayout import FloatLayout
+
+# from piece_selection import PieceSelection
+from piece_service import plie_piece
+from kivy.core.audio import SoundLoader
+
+Builder.load_file("playback.kv")
+
+
+class Playback(FloatLayout):
+    play_enabled = BooleanProperty(True)
+    stop_enabled = BooleanProperty(True)
+
+    def __init__(self, **kwargs):
+        super(Playback, self).__init__(**kwargs)
+        self.play_enabled = True
+        self.stop_enabled = False
+        '''if plie_piece.displayname is not None:
+            plie_piece_title_label = Label(text=str(plie_piece.displayname),
+                                           pos_hint={'center_x': 0.5, 'center_y': .96},
+                                           size_hint=(1, 1),
+                                           font_size=30
+                                           )
+
+            self.add_widget(plie_piece_title_label)
+            plie_piece_bpm_label = Label(text=str(plie_piece.bpm),
+                                         pos_hint={'center_x': 0.5, 'center_y': .96},
+                                         size_hint=(1, 1),
+                                         font_size=30
+            )
+            self.add_widget(plie_piece_bpm_label)'''
+
+
+    def play_plie_piece(self):
+        if plie_piece.filename is None:
+            pass
+        else:
+            self.sound = SoundLoader.load(plie_piece.filename)
+            self.sound.play()
+            self.play_enabled = False
+            self.stop_enabled = True
+
+    def stop_audio(self):
+        if plie_piece.filename is None:
+            pass
+        else:
+            self.sound.stop()
+            self.play_enabled = True
+            self.stop_enabled = False
+
+
